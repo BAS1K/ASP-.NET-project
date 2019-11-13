c3bd10012984d1c003f4af61064c67f33d8bbbbd
Danylo <33275488+BAS1K@users.noreply.github.com>
2019-11-13 22:49:19 +0200

    added other files

---
+++ TVStore/SampleData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TVStore.Models;
+
+namespace TVStore
+{
+    public static class SampleData
+    {
+        public static void Initialize(TVContext context)
+        {
+            if (!context.TVs.Any())
+            {
+                context.TVs.AddRange(
+                    new TV
+                    {
+                        Name = "LG X54T-2019",
+                        Company = "LG",
+                        Price = 1000
+                    },
+                    new TV
+                    {
+                        Name = "Samsung UltraTV-v.3",
+                        Company = "Samsung",
+                        Price = 950
+                    },
+                    new TV
+                    {
+                        Name = "Sony XD-555",
+                        Company = "Sony",
+                        Price = 660
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
